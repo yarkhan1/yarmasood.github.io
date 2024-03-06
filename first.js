0e56c1ef9396db022fd1772598f1dfc1f7c4da3f
uzairjanmalik <muhammaduzair1528@gmail.com>
2024-03-06 20:42:56 +0500

    first

---
+++ first.js
@@ -0,0 +1,26 @@
+ 
+  function calculateChickenPrice() {
+    let weight = parseFloat(prompt("Kitnay gram chicken lena hai?"));
+    let pricePerKg = 500;
+    let price = (weight / 1000) * pricePerKg;
+     document.write(`<h1>  Aapko ${weight} gram chicken ke liye ${price} rupay denay honge. </h1> `);
+}
+
+calculateChickenPrice();ea
+ 
+ 
+ 
+// let rr = [
+
+//   one1 ={
+//   name : "yar khan massod",
+//   father : "ramajan khan",
+//   age : 20,
+//   city : "wazetristan "
+//   khan2 = [ 1,2,3 ],
+  
+  
+//   }
+  
+//   ]
+//   console.log (rr[0])
